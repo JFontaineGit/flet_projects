ef1c17bd7f8269d88b28a139d8748317f5152970
Just Fontaine <itsmealee@gmail.com>
2025-03-16 01:34:12 -0300

    (fix) 'UserControl' obsoleto, corregido

--- password_generator_app/password_generator.py
+++ password_generator_app/password_generator.py
@@ -1,4 +1,4 @@
-from flet import *
+import flet as ft
 import secrets
 import string
 import pyperclip
@@ -6,82 +6,105 @@ import pyperclip
 random = secrets.SystemRandom()
 contraseñas = []
 
-class TitleContainer(UserControl):
-    def build(self):
-        return Container(
+class PasswordGeneratorApp:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.setup_page()
+        self.setup_ui()
+        self.add_to_page()
+
+    def setup_page(self):
+        self.page.title = "Password Generator"
+        self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        self.page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+        self.page.window_resizable = False
+        self.page.window_maximizable = False
+        self.page.window_width = 330
+
+    def setup_ui(self):
+        self.title_container = ft.Container(
             width=275,
             height=60,
-            content=Column(
+            content=ft.Column(
                 spacing=5,
-                horizontal_alignment=CrossAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                 controls=[
-                    Text(
+                    ft.Text(
                         "Password Generator",
                         size=25,
-                        weight=FontWeight.BOLD,
+                        weight=ft.FontWeight.BOLD,
                     ),
                 ],
             ),
         )
 
-class MainContainer(UserControl):
-    def __init__(self, funcion, funcion2):
-        super().__init__()
-        self.funcion = funcion
-        self.funcion2 = funcion2
-        self.text_field = TextField(
-            border=InputBorder.UNDERLINE,
+        self.text_field = ft.TextField(
+            border=ft.InputBorder.UNDERLINE,
             width=205,
             read_only=True,
             hint_text="Your Password",
         )
-        self.copy_button = IconButton(
-            icon=icons.CONTENT_COPY,
+        self.copy_button = ft.IconButton(
+            icon=ft.icons.CONTENT_COPY,
             disabled=True,
-            on_click=funcion2
+            on_click=self.copy_function
         )
 
-    def text_field_value(self, long):
-        self.funcion(long)
+        self.buttons_row = ft.Row(
+            [
+                ft.FilledButton("12", on_click=lambda e: self.text_field_value(12), style=ft.ButtonStyle(shape=ft.CircleBorder(), padding=30)),
+                ft.FilledButton("16", on_click=lambda e: self.text_field_value(16), style=ft.ButtonStyle(shape=ft.CircleBorder(), padding=30)),
+                ft.FilledButton("20", on_click=lambda e: self.text_field_value(20), style=ft.ButtonStyle(shape=ft.CircleBorder(), padding=30)),
+            ],
+        )
+
+        self.main_container = ft.Container(
+            width=275,
+            height=200,
+            clip_behavior=ft.ClipBehavior.HARD_EDGE,
+            content=ft.Column(
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                controls=[
+                    ft.Row(
+                        [
+                            self.text_field,
+                            self.copy_button
+                        ],
+                    ),
+                    ft.Divider(height=20, color="transparent"),
+                    self.buttons_row
+                ],
+            ),
+        )
+
+    def add_to_page(self):
+        self.page.add(
+            ft.Container(
+                width=300,
+                height=600,
+                padding=20,
+                border_radius=40,
+                border=ft.border.all(1, ft.colors.WHITE),
+                content=ft.Column(
+                    controls=[
+                        ft.Divider(height=20, color="transparent"),
+                        self.title_container,
+                        ft.Divider(height=30, color="transparent"),
+                        self.main_container
+                    ]
+                )
+            )
+        )
+        self.page.update()
+
+    def text_field_value(self, longitud):
+        self.password(longitud)
         self.copy_button.disabled = False
         self.copy_button.update()
         self.text_field.value = contraseñas[0]
         self.text_field.update()
 
-    def Buttons(self) -> Control:
-        return Row(
-            [
-                FilledButton("12", on_click=lambda e: self.text_field_value(12), style=ButtonStyle(shape=CircleBorder(), padding=30)),
-                FilledButton("16", on_click=lambda e: self.text_field_value(16), style=ButtonStyle(shape=CircleBorder(), padding=30)),
-                FilledButton("20", on_click=lambda e: self.text_field_value(20), style=ButtonStyle(shape=CircleBorder(), padding=30)),
-            ],
-        )
-
-    def TextField(self) -> Control:
-        return Row(
-            [
-                self.text_field,
-                self.copy_button
-            ],
-        )
-
-    def build(self):
-        return Container(
-            width=275,
-            height=200,
-            clip_behavior=ClipBehavior.HARD_EDGE,
-            content=Column(
-                horizontal_alignment=CrossAxisAlignment.CENTER,
-                controls=[
-                    self.TextField(),
-                    Divider(height=20, color="transparent"),
-                    self.Buttons()
-                ],
-            ),
-        )
-
-def main(page: Page):
-    def password(longitud):
+    def password(self, longitud):
         caract_esp = "#$%&'()*+-/:;<=>?@[\]^_`{|}~"
         contra_seg = string.ascii_letters + string.digits + caract_esp
         while True:
@@ -95,37 +118,14 @@ def main(page: Page):
                 contraseñas.append(it_seg)
                 break
 
-    def copy_function(e):
+    def copy_function(self, e):
         pyperclip.copy(contraseñas[0])
-        page.snack_bar = SnackBar(Text("Contraseña copiada al portapapeles"))
-        page.snack_bar.open = True
-        page.update()
+        self.page.snack_bar = ft.SnackBar(ft.Text("Contraseña copiada al portapapeles"))
+        self.page.snack_bar.open = True
+        self.page.update()
 
-    page.title = "Password Generator"
-    page.vertical_alignment = MainAxisAlignment.CENTER
-    page.horizontal_alignment = MainAxisAlignment.CENTER
-    page.window_resizable = False
-    page.window_maximizable = False
-    page.window_width = 330
-
-    main_container = Container(
-        width=300,
-        height=600,
-        padding=20,
-        border_radius=40,
-        border=border.all(1, colors.WHITE),
-        content=Column(
-            controls=[
-                Divider(height=20, color="transparent"),
-                TitleContainer(),
-                Divider(height=30, color="transparent"),
-                MainContainer(password, copy_function)
-            ]
-        )
-    )
-
-    page.add(main_container)
-    page.update()
+def main(page: ft.Page):
+    PasswordGeneratorApp(page)
 
 if __name__ == "__main__":
-    app(target=main)
+    ft.app(target=main)
